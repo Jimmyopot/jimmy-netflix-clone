04fda998b8fab5d666182af7e8a6896cec9497bb
Jimmyopot <jimmyopot@gmail.com>
2021-04-12 11:24:52 +0200

    Excited about builing a netflix/movie app, i love Netflix UI and UX

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
-import logo from './logo.svg';
+// 7aae87a5404509f6910be7133f4b20f5
+// https://api.themoviedb.org/3/movie/550?api_key=7aae87a5404509f6910be7133f4b20f5
+import React from 'react';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Netflix Build!</h1>
     </div>
   );
 }
