6fe47dbbebb37809889b6255315d836cbde07e7e
Jimmyopot <jimmyopot@gmail.com>
2021-04-13 18:17:28 +0200

    Styled the app with beautiful rows, catch your movie

--- src/App.js
+++ src/App.js
@@ -2,11 +2,28 @@
 // https://api.themoviedb.org/3/movie/550?api_key=7aae87a5404509f6910be7133f4b20f5
 import React from 'react';
 import './App.css';
+import Row from './components/Row';
+import Banner from './components/Banner';
+import requests from './requests';
 
 function App() {
   return (
     <div className="App">
-      <h1>Netflix Build!</h1>
+    {/* Navbar, header */}
+    <Banner />
+      
+      <Row 
+          title="NETFLIX ORIGINALS" 
+          fetchUrl={requests.fetchNetflixOriginals} 
+          isLargeRow={true}
+      />
+      <Row title="Trending Now" fetchUrl={requests.fetchTrending} />
+      <Row title="Top Rated" fetchUrl={requests.fetchTopRated} />
+      <Row title="Action Movies" fetchUrl={requests.fetchActionMovies} />
+      <Row title="Comedy Movies" fetchUrl={requests.fetchComedyMovies} />
+      <Row title="Horror Movies" fetchUrl={requests.fetchHorrorMovies} />
+      <Row title="Romance Movies" fetchUrl={requests.fetchRomanceMovies} />
+      <Row title="Documentaries" fetchUrl={requests.fetchDocumentaries} />
     </div>
   );
 }
