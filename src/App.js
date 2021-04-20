3465445b20a9cc986c357a983506c26bb95262a8
Jimmyopot <jimmyopot@gmail.com>
2021-04-20 17:55:09 +0200

    Modified read me file

--- src/App.js
+++ src/App.js
@@ -1,5 +1,3 @@
-// 7aae87a5404509f6910be7133f4b20f5
-// https://api.themoviedb.org/3/movie/550?api_key=7aae87a5404509f6910be7133f4b20f5
 import React from 'react';
 import './App.css';
 import Row from './components/Row';
